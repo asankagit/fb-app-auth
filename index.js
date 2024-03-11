eef601fcfed9a9e4b1b073676617cfad5a0b579d
Asanka Basnayake <inbox2asanka@gmail.com>
2024-03-11 17:24:36 +0530

    Passport initialization issue fix

--- index.js
+++ index.js
@@ -3,50 +3,44 @@ const https = require('node:https');
 const http = require('http')
 const fs = require('node:fs');
 const passport = require('passport');
-const FacebookStrategy = require('passport-facebook');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 require('dotenv').config();
 const routes = require("./routes");
-console.log({ routes })
+const session = require("express-session");
+
 
 const app = express();
 
-const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
-const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.set("port", 3000);
+app.set("port", 3000);
 const options = {
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem'),
-  passphrase: "123456",
+  key: fs.readFileSync('./certificates/key.pem'),
+  cert: fs.readFileSync('./certificates/cert.pem'),
+  passphrase: process.env['PASSPHRASE']
 };
 
 
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/", routes )
-
-passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "https://localhost:8000/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
 
 
 http.createServer(app).listen(3000); 
--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 
 require('dotenv').config();
@@ -13,41 +13,8 @@ passport.use(new FacebookStrategy({
   callbackURL: 'https://localhost:8000/auth/facebook/callback',
   state: true
 }, function verify(accessToken, refreshToken, profile, cb) {
-
-  console.log("this is calling")
-  return cb(accessToken, refreshToken, profile)
-  // db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-  //   'https://www.facebook.com',
-  //   profile.id
-  // ], function(err, row) {
-  //   if (err) { return cb(err); }
-  //   if (!row) {
-  //     db.run('INSERT INTO users (name) VALUES (?)', [
-  //       profile.displayName
-  //     ], function(err) {
-  //       if (err) { return cb(err); }
-  //       var id = this.lastID;
-  //       db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-  //         id,
-  //         'https://www.facebook.com',
-  //         profile.id
-  //       ], function(err) {
-  //         if (err) { return cb(err); }
-  //         var user = {
-  //           id: id,
-  //           name: profile.displayName
-  //         };
-  //         return cb(null, user);
-  //       });
-  //     });
-  //   } else {
-  //     db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
-  //       if (err) { return cb(err); }
-  //       if (!row) { return cb(null, false); }
-  //       return cb(null, row);
-  //     });
-  //   }
-  // });
+  const user = { accessToken, refreshToken, profile }
+  return cb(null, user)
 }));
 
 passport.serializeUser(function (user, cb) {
@@ -68,24 +35,22 @@ passport.deserializeUser(function (user, cb) {
 var router = express.Router();
 router.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function (req, res) {
-    // Successful authentication, redirect home.
+  async (req, res) => {
+    console.log("user", req.user)
     res.redirect('/success');
   }
 );
 
 
 router.get('/login', function (req, res, next) {
-  res.render('login', { hasMessages: true, messages: [1] });
+  res.render('login', { hasMessages: true, messages: ['Facebook login integration - demo'] });
 });
 
 router.get('/login/federated/facebook', passport.authenticate('facebook'));
 
-router.get('/oauth2/redirect/facebook', passport.authenticate('facebook', 
-{
-  successReturnToOrRedirect: '/',
-  failureRedirect: '/login'
-}));
+router.get('/oauth2/redirect/facebook', passport.authenticate('facebook',
+  { scope: ['business_management', 'pages_show_list', 'instagram_basic', 'pages_read_engagement'] }
+));
 
 router.post('/logout', function (req, res, next) {
   req.logout(function (err) {
@@ -94,14 +59,6 @@ router.post('/logout', function (req, res, next) {
   });
 });
 
-
-router.get('/user-token/:token', async function (req, res, next) {
-  const token = req.params.token;
-  const result =  await fetch(`https://graph.facebook.com/v19.0/me?fields=accounts&access_token=${token}`).then(res => res.json())
-
-  console.log(result)
-  res.send(JSON.stringify(result))
-});
 
 
 
